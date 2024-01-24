4225d21189fd4f750e8f103c5dd5c27a5ab5dfad
Rebecca <rebeayto@gmail.com>
2024-01-24 15:57:55 +0000

    rename tool basic list population of all videos in a folder when one picked

---
+++ MyFlix/RenameTool/RenameToolVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFlix.RenameTool
+{
+    public class RenameToolVideo
+    {
+        public string fileName { get; set; }
+        public string filePath { get; set; }
+        public string newName { get; set; }
+    }
+}
---
+++ MyFlix/ViewModels/RenameToolViewModel.cs
@@ -0,0 +1,61 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MyFlix.RenameTool;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace MyFlix.ViewModels
+{
+    public class RenameToolViewModel
+    {
+        public string SearchRegex { get; set; }
+        public string ReplaceRegex { get; set; }
+
+        public ICommand folderPickerCommand { get; }
+
+        public ObservableCollection<RenameToolVideo> renamingVideos { get; set; }
+
+        public RenameToolViewModel() {
+            folderPickerCommand = new RelayCommand(FolderPicker);
+            renamingVideos = new ObservableCollection<RenameToolVideo>();
+        }
+
+        private void FolderPicker()
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new()
+            {
+                //options here
+            };
+
+            DialogResult result = dialog.ShowDialog();
+
+            if (result != System.Windows.Forms.DialogResult.OK)
+            {
+                //If user cancelled out
+                return;
+            }
+
+            string rootFilePath = dialog.SelectedPath;
+
+            FileSystemSearcher searcher = new();
+            searcher.GetVideosInDirRecursively(rootFilePath);
+
+            List<FileSystemVideo> videos = searcher.videos;
+
+            //TODO: calculate regex search/replace effect and set RenameToolVideo.newName 
+
+            foreach(FileSystemVideo video in videos)
+            {
+                renamingVideos.Add(new RenameToolVideo() { 
+                    fileName = video.fileName,
+                    filePath = video.filePath,
+                });
+            }
+        }
+    }
+}
