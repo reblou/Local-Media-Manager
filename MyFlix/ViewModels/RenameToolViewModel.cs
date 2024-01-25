9a954dd43dc0c0060f888b87ae6424465e48ebb8
Rebecca <rebeayto@gmail.com>
2024-01-25 12:17:47 +0000

    rename tool can do basic Regex replace renaming functionality of videos in a folder

--- MyFlix/ViewModels/RenameToolViewModel.cs
+++ MyFlix/ViewModels/RenameToolViewModel.cs
@@ -3,8 +3,10 @@ using MyFlix.RenameTool;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
@@ -17,12 +19,20 @@ namespace MyFlix.ViewModels
         public string ReplaceRegex { get; set; }
 
         public ICommand folderPickerCommand { get; }
+        public ICommand renameCommand { get; }
+        public ICommand undoCommand { get; }
 
         public ObservableCollection<RenameToolVideo> renamingVideos { get; set; }
 
         public RenameToolViewModel() {
             folderPickerCommand = new RelayCommand(FolderPicker);
+            renameCommand = new RelayCommand(RenameCommand);
+            undoCommand = new RelayCommand(UndoCommand);
+
             renamingVideos = new ObservableCollection<RenameToolVideo>();
+
+            SearchRegex = "";
+            ReplaceRegex = "";
         }
 
         private void FolderPicker()
@@ -40,6 +50,8 @@ namespace MyFlix.ViewModels
                 return;
             }
 
+            renamingVideos.Clear();
+
             string rootFilePath = dialog.SelectedPath;
 
             FileSystemSearcher searcher = new();
@@ -47,15 +59,39 @@ namespace MyFlix.ViewModels
 
             List<FileSystemVideo> videos = searcher.videos;
 
-            //TODO: calculate regex search/replace effect and set RenameToolVideo.newName 
+            //TODO: calculate every time input text changed ?
 
             foreach(FileSystemVideo video in videos)
             {
+                //TODO: error handling of invalid regex formats etc.
+                string newName = Regex.Replace(video.fileName, SearchRegex, ReplaceRegex);
+
                 renamingVideos.Add(new RenameToolVideo() { 
                     fileName = video.fileName,
                     filePath = video.filePath,
+                    newName = newName,
                 });
             }
+        }
+
+        private void RenameCommand()
+        {
+            foreach (RenameToolVideo video in renamingVideos)
+            {
+                if (video.fileName == video.newName) continue;
+
+                string newPath = Path.Combine(Path.GetDirectoryName(video.filePath), video.newName);
+
+                //TODO: error handling 
+                File.Move(video.filePath, newPath);
+            }
+        }
+
+        private void UndoCommand()
+        {
+            // go through rename videos 
+
+            // rename newName -> filename
         }
     }
 }
